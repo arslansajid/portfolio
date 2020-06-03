42a0a5fb1634109fe14a2f9942d932225bd26f19
Arslan Sajid <arslansajid951@gmail.com>
2020-06-04 00:46:35 +0500

    Added global colors

---
+++ src/styles/Colors.js
@@ -0,0 +1,16 @@
+const Colors = {
+    white: "#FFFFFF",
+    black: "#2D2D2D",
+    brandColor: '#FFDBE7',
+    border: "#3D35D1",
+    headingBlue: "#102B5C",
+    textBlack: "#2D2D2D",
+    textGrey: "#A1A4AC",
+    appRed: "#FF5E6B",
+    appPink: "#FF77A4",
+    appPinkLite: "#FF9CBD",
+    seperator: "#E2E2E2"
+  };
+  
+  export default Colors;
+  
