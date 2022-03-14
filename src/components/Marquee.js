87963406deb267835a45baa90f9f3c7384f75a30
Arslan Sajid <arslansajid951@gmail.com>
2022-03-14 16:31:51 +0100

    Added marquee component

---
+++ src/components/Marquee.js
@@ -0,0 +1,53 @@
+import React from "react";
+import Marquee from "react-fast-marquee";
+import Technologies from "../static/Technologies"
+import { makeStyles } from '@material-ui/core';
+
+const Block = ({ name, imgUrl, className }) => (
+    <div className={className}>
+        <img alt={name} src={imgUrl} />
+    </div>
+)
+
+const TechMarquee = () => {
+    const classes = useStyles();
+
+    return (
+        <div className={classes.marqueeContainer}>
+            <Marquee speed={50}>
+                {Technologies.map((tech, index) => (
+                    <div key={`${tech.name}_${index}`}>
+                        <Block className={classes.block} name={tech.name} imgUrl={tech.image} />
+                    </div>
+                ))}
+            </Marquee>
+        </div>
+    )
+};
+
+
+
+const useStyles = makeStyles((theme) => ({
+    marqueeContainer: {
+        padding: "2em 0",
+    },
+    block: {
+        width: 100,
+        height: 100,
+        padding: "10px 30px",
+
+        [theme.breakpoints.down('sm')]: {
+            width: 60,
+            height: 60,
+            padding: "10px 20px",
+        },
+
+        "& img": {
+            width: "100%",
+            height: "100%",
+            objectFit: "contain",
+        }
+    },
+}));
+
+export default TechMarquee;
---
+++ src/static/Technologies.js
@@ -0,0 +1,60 @@
+const Technologies = [
+    {
+        name: "HTML 5",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/2048px-HTML5_logo_and_wordmark.svg.png",
+    },
+    {
+        name: "CSS 3",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1200px-CSS3_logo_and_wordmark.svg.png",
+    },
+    {
+        name: "Bootstrap",
+        image: "https://cdn.worldvectorlogo.com/logos/bootstrap-4.svg",
+    },
+    {
+        name: "Javascript",
+        image: "https://cdn.worldvectorlogo.com/logos/logo-javascript.svg",
+    },
+    {
+        name: "Typescript",
+        image: "https://cdn.worldvectorlogo.com/logos/typescript.svg",
+    },
+    {
+        name: "React",
+        image: "https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg",
+    },
+    {
+        name: "Graph-QL",
+        image: "https://cdn.worldvectorlogo.com/logos/graphql.svg",
+    },
+    // {
+    //     name: "React-Native",
+    //     image: "https://cdn.worldvectorlogo.com/logos/react-native-1.svg",
+    // },
+    {
+        name: "Node.JS",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Node.js_logo.svg/1280px-Node.js_logo.svg.png",
+    },
+    {
+        name: "Redux",
+        image: "https://cdn.worldvectorlogo.com/logos/redux.svg",
+    },
+    {
+        name: "Firebase",
+        image: "https://cdn.worldvectorlogo.com/logos/firebase-1.svg",
+    },
+    {
+        name: "Gitub",
+        image: "https://cdn.worldvectorlogo.com/logos/git-icon.svg",
+    },
+    {
+        name: "Material UI",
+        image: "https://cdn.worldvectorlogo.com/logos/material-ui-1.svg",
+    },
+    {
+        name: "Semantic UI",
+        image: "https://cdn.worldvectorlogo.com/logos/semantic-ui.svg",
+    },
+]
+
+export default Technologies;
